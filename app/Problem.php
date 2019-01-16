0bc917b3086577721955914c0c8b8914bca58f53
jsice <wiwadh.c@ku.th>
2019-01-16 13:52:06 +0700

    update model and migration files

--- app/Problem.php
+++ app/Problem.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Problem extends Model
 {
-    //
+    protected $fillable = [
+        'admin_id', 'name', 'pdf_path'
+    ];
+
+    public function testsets() {
+		return $this->hasMany('App\ProblemTestSet', 'problem_id');
+	}
+
 }
