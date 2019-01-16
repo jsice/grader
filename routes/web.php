fea752172b660fbc51e81a460625c21192e94845
jsice <wiwadh.c@ku.th>
2019-01-16 15:42:09 +0700

    add more route

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/problems', 'ProblemsController@index');
 Route::get('/problems/{id}', 'ProblemsController@show');
+Route::post('/problems', 'ProblemsController@create');
 Route::put('/problems/{id}', 'ProblemsController@update');
 
 Route::get('/scoreboard', 'ScoreboardController@index');
