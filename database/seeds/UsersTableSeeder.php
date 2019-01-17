da6c88071fc86acf472e979a64c47f27d8040cc6
jsice <wiwadh.c@ku.th>
2019-01-18 02:07:05 +0700

    seed student type user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,13 @@ class UsersTableSeeder extends Seeder
         $admin->type = 'admin';
         $admin->std_id = '5810400051';
         $admin->save();
+        $admin = new App\User;
+        $admin->password = bcrypt('userpassword');
+        $admin->name = 'ice2';
+        $admin->email = 'user@ku.th';
+        $admin->type = 'student';
+        $admin->std_id = '6010400051';
+        $admin->save();
         factory(App\User::class, 50)->create();
     }
 }
