f28d716febd7756e246a45c2044ed6c7d01d4e56
jutinapas <jutinapas@gmail.com>
2019-01-17 17:55:41 +0700

    implement storage for icon siam

--- resources/views/scoreboard.blade.php
+++ resources/views/scoreboard.blade.php
@@ -5,12 +5,13 @@ Scoreboard
 @endsection
 
 @section('content')
+{{ print_r($scoreboard) }}
   @if (count($scoreboard) == 0)
   <h3 class="display-5 text-muted text-center mt-5">- SCOREBOARD EMPTY -<h3>
     @else
     <table class="table table-hover">
       <thead>
-        <tr class="table-primary">
+        <tr class="table-dark">
             <th scope="col">Rank</th>
             <th scope="col">ID</th>
             <th scope="col">Name</th>
@@ -18,18 +19,26 @@ Scoreboard
             <th scope="col">{{ $problem->name }}</th>
             @endforeach
             <th scope="col">Total</th>
-          </tr>
+        </tr>
         </thead>
         <tbody>
           <?php $rank = 1 ?>
           @foreach($scoreboard as $key => $scores)
           <?php $user = \App\User::where('id',$key)->first() ?>
-          <tr class="table-light">
+          <tr>
             <td score="row">{{ $rank }}</td>
             <td scope="row">{{ $user->std_id }}</td>
             <td scope="row">{{ $user->name }}</td>
             @foreach($scores as $key => $score)
-                <td scope="row">{{ $score }}</td>
+              <td scope="row" align="center">
+              @if ($score == -1)
+                -
+              @elseif ($score == 0)
+                <img src="icons/wrong.png" />
+              @elseif ($score == 1)
+                <img src="icons/correct.png" />
+              @endif
+              </td>
             @endforeach
             <?php $rank++ ?>
           </tr>
--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,19 @@ Route::post('/submissions', 'SubmissionsController@store');
 Route::get('/submissions/{id}/edit', 'ProblemsController@edit');
 Route::put('/submissions/{id}', 'ProblemsController@update');
 
+Route::get('{folder}/{filename}', function ($folder, $filename)
+{
+    $path = storage_path('app/public/'.$folder.'/' . $filename);
+    if (!File::exists($path)) {
+        abort(404);
+    }
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+
+
+});
