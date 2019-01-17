18957021f25d95742d61e90483561fbbe2e60851
jsice <wiwadh.c@ku.th>
2019-01-17 12:19:50 +0700

    remove DB_DATABASE in .env; change db file name

--- app/Http/Controllers/ScoreboardController.php
+++ app/Http/Controllers/ScoreboardController.php
@@ -3,28 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use \App\User;
 class ScoreboardController extends Controller
 {
     public function index() {
         $userScores = Array();
         $users = \App\User::all();
-        foreach ($users as $user){
-            $id = $user->id;
-            $userScores[$id] = 0;
-        }
+        // foreach ($users as $user){
+        //     $id = $user->id;
+        //     $userScores[$id] = 0;
+        // }
 
-        $submissions = \App\Submission::all();
-        foreach ($submissions as $submission){
-            if ($submission->status == ''){
-                $userScores[$submission->user_id] += 1;
-            }
-        }
+        // $submissions = \App\Submission::all();
+        // foreach ($submissions as $submission){
+        //     if ($submission->status == ''){
+        //         $userScores[$submission->user_id] += 1;
+        //     }
+        // }
 
-        arsort($userScores);
+        // arsort($userScores);
 
-        foreach ($userScores as $userScore){
+        // foreach ($userScores as $userScore){
             
-        }
+        // }
+        return "wow";
     }
 }
