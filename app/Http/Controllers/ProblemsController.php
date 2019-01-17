87e32f6049530cb78a2d980cf42ddb863816df68
Unknown <ittikorn.c@ku.th>
2019-01-17 12:02:44 +0700

    Modify Problems Controllers

--- app/Http/Controllers/ProblemsController.php
+++ app/Http/Controllers/ProblemsController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Problem
 
 class ProblemsController extends Controller
 {
-    //
+    public function index() {
+        $problems = \App\Problem::all();
+        return view('problems.index', ['problems' => $problems]);
+    }
+
+    public function show($id){
+        $problem = Problem::findOrFail($id);
+        return view('problems.show', ['problem' => $problem]);
+    }
+
+    public function update(){
+
+    }
 }
--- app/Http/Controllers/ScoreboardController.php
+++ app/Http/Controllers/ScoreboardController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class ScoreboardController extends Controller
 {
-    //
+    public function index() {
+        $userScores = Array();
+        $users = \App\User::all();
+        foreach ($users as $user){
+            $id = $user->id;
+            $userScores[$id] = 0;
+        }
+
+        $submissions = \App\Submission::all();
+        foreach ($submissions as $submission){
+            if ($submission->status == ''){
+                $userScores[$submission->user_id] += 1;
+            }
+        }
+
+        arsort($userScores);
+
+        foreach ($userScores as $userScore){
+            
+        }
+    }
 }
