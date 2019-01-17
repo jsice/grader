372f3cd2a505b90b044d8ca2d54af67f1d73765e
jutinapas <jutinapas@gmail.com>
2019-01-17 19:08:44 +0700

    fix scoreboard alignment

--- resources/views/scoreboard.blade.php
+++ resources/views/scoreboard.blade.php
@@ -5,38 +5,45 @@ Scoreboard
 @endsection
 
 @section('content')
-{{ print_r($scoreboard) }}
   @if (count($scoreboard) == 0)
   <h3 class="display-5 text-muted text-center mt-5">- SCOREBOARD EMPTY -<h3>
-    @else
+  @else
     <table class="table table-hover">
       <thead>
         <tr class="table-dark">
-            <th scope="col">Rank</th>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
+            <th style="text-align: center;">Rank</th>
+            <th style="text-align: center;">ID</th>
+            <th>Name</th>
             @foreach($problems as $problem)
-            <th scope="col">{{ $problem->name }}</th>
+            <th style="text-align: center;">{{ $problem->name }}</th>
             @endforeach
-            <th scope="col">Total</th>
+            <th style="text-align: center;">Total</th>
         </tr>
         </thead>
         <tbody>
-          <?php $rank = 1 ?>
+          <?php
+            $rank = 1
+          ?>
           @foreach($scoreboard as $key => $scores)
-          <?php $user = \App\User::where('id',$key)->first() ?>
+          <?php
+            $user = \App\User::where('id',$key)->first();
+          ?>
           <tr>
-            <td score="row">{{ $rank }}</td>
-            <td scope="row">{{ $user->std_id }}</td>
+            <td score="row" align="center">{{ $rank }}</td>
+            <td scope="row" align="center">{{ $user->std_id }}</td>
             <td scope="row">{{ $user->name }}</td>
             @foreach($scores as $key => $score)
               <td scope="row" align="center">
-              @if ($score == -1)
+              @if ($key == "total")
+                {{$score}}
+              @else
+                @if ($score == -1)
                 -
-              @elseif ($score == 0)
+                @elseif ($score == 0)
                 <img src="icons/wrong.png" />
-              @elseif ($score == 1)
+                @elseif ($score == 1)
                 <img src="icons/correct.png" />
+                @endif
               @endif
               </td>
             @endforeach
