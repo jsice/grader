64e9ad1af17128bd3057f20785e5a94b47bf0381
vudhidej-d <vudhidej.d@ku.th>
2019-01-18 18:03:34 +0700

    remove unused code, edit edit func, edit update func,

--- app/Http/Controllers/SubmissionsController.php
+++ app/Http/Controllers/SubmissionsController.php
@@ -47,26 +47,6 @@ class SubmissionsController extends Controller
         $submission->file_path = $fileName;
         $submission->save();
 
-        //Display File Name
-        echo 'File Name: '.$codeFile->getClientOriginalName();
-        echo '<br>';
-   
-        //Display File Extension
-        echo 'File Extension: '.$codeFile->getClientOriginalExtension();
-        echo '<br>';
-    
-        //Display File Real Path
-        echo 'File Real Path: '.$codeFile->getRealPath();
-        echo '<br>';
-    
-        //Display File Size
-        echo 'File Size: '.$codeFile->getSize();
-        echo '<br>';
-    
-        //Display File Mime Type
-        echo 'File Mime Type: '.$codeFile->getMimeType();
-
-        //Move Uploaded File
         $destinationPath = 'submissions';
         $codeFile->storeAs($destinationPath, $fileName);
 
@@ -81,13 +61,15 @@ class SubmissionsController extends Controller
         return view('submissions.show', ['submission' => $submission]);
     }
 
-    public function edit(Submission $submission, $id)
+    public function edit($id)
     {
+        $submission = Submission::findOrFail($id);
         return view('submissions.edit', compact('submission', 'id'));
     }
     
-    public function update(Request $request, Submission $submission)
+    public function update(Request $request, $id)
     {
+        $submission = Submission::findOrFail($id);
         $submission->status = $request->input('status');
         $submission->save();
         return redirect('submissions/'.$submission->id);
