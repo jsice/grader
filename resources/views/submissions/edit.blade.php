2c67cd1bca630a9585bc15dd4384200b7e5e3b44
vudhidej-d <vudhidej.d@ku.th>
2019-01-18 18:07:13 +0700

    set method and action for form, render form by loop for handle checked input (default value), back button can back

--- resources/views/submissions/edit.blade.php
+++ resources/views/submissions/edit.blade.php
@@ -15,45 +15,32 @@
   </style>
 @endpush
 @section('content')
+  <?php
+    $status_list = ['YES', 'NO:TimeLimitExceed', 'NO:CompilationError', 'NO:RunTimeError', 'NO:WrongAnswer',
+    'NO:ContactTA', 'DELETED', 'PENDING']
+  ?>
   <h3 class="display-4" style="font-size: 2.5rem;">Edit Status</h3>
-  <form>
+  <form method="post" action="{{ '/submissions/' . $id }}">
+    {{ csrf_field() }}
+    {{ method_field('PUT') }}
     <div class="form-group">
-      <div class="row custom-control custom-radio custom-control-inline">
-        <input type="radio" class="custom-control-input" name="status" id="YES" value="YES">
-        <label class="custom-control-label" for="YES">YES</label>
-      </div>
-      <div class="row custom-control custom-radio custom-control-inline">
-        <input type="radio" class="custom-control-input" name="status" id="NO:TimeLimitExceed" value="NO:TimeLimitExceed">
-        <label class="custom-control-label" for="NO:TimeLimitExceed">NO:TimeLimitExceed</label>
-      </div>
-      <div class="row custom-control custom-radio custom-control-inline">
-        <input type="radio" class="custom-control-input" name="status" id="NO:CompilationError" value="NO:CompilationError">
-        <label class="custom-control-label" for="NO:CompilationError">NO:CompilationError</label>
-      </div>
-      <div class="row custom-control custom-radio custom-control-inline">
-        <input type="radio" class="custom-control-input" name="status" id="NO:RunTimeError" value="NO:RunTimeError">
-        <label class="custom-control-label" for="NO:RunTimeError">NO:RunTimeError</label>
-      </div>
-      <div class="row custom-control custom-radio custom-control-inline">
-        <input type="radio" class="custom-control-input" name="status" id="NO:WrongAnswer" value="NO:WrongAnswer">
-        <label class="custom-control-label" for="NO:WrongAnswer">NO:WrongAnswer</label>
-      </div>
-      <div class="row custom-control custom-radio custom-control-inline">
-        <input type="radio" class="custom-control-input" name="status" id="NO:ContactTA" value="NO:ContactTA">
-        <label class="custom-control-label" for="NO:ContactTA">NO:ContactTA</label>
-      </div>
-      <div class="row custom-control custom-radio custom-control-inline">
-        <input type="radio" class="custom-control-input" name="status" id="DELETED" value="DELETED">
-        <label class="custom-control-label" for="DELETED">DELETED</label>
-      </div>
-      <div class="row custom-control custom-radio custom-control-inline">
-        <input type="radio" class="custom-control-input" name="status" id="PENDING" value="PENDING">
-        <label class="custom-control-label" for="PENDING">PENDING</label>
-      </div>
+      @foreach ($status_list as $status)
+        @if ($submission->status === $status)
+          <div class="row custom-control custom-radio custom-control-inline">
+            <input checked type="radio" class="custom-control-input" name="status" id="{{ $status }}" value="{{ $status }}">
+            <label class="custom-control-label" for="{{ $status }}">{{ $status }}</label>
+          </div>
+        @else
+          <div class="row custom-control custom-radio custom-control-inline">
+            <input type="radio" class="custom-control-input" name="status" id="{{ $status }}" value="{{ $status }}">
+            <label class="custom-control-label" for="{{ $status }}">{{ $status }}</label>
+          </div>
+        @endif
+      @endforeach
       <div class="row" id="btn-row">
         <div class="col-10"></div>
         <div class="col-1">
-          <a role=button class="btn btn-outline-danger" href="">Cancel</a>
+          <a role=button class="btn btn-outline-danger" href="javascript:history.back()">Cancel</a>
         </div>
         <div class="col-1">
           <button type="submit" class="btn btn-info">Submit</button>
