e4a5f6b90035556aac704cee8459ec3e62a3b351
Ton Vudhidej <vudhidej@muze.co.th>
2019-01-23 23:10:45 +0700

    show submission status on problem

--- resources/views/problems/index.blade.php
+++ resources/views/problems/index.blade.php
@@ -21,6 +21,10 @@ Problems
   @endif
 @endsection
 @section('content')
+    <?php
+      $color = "black";
+      $status = "-";
+    ?>
     <table class="table table-hover">
       <thead>
         <tr class="table-dark">
@@ -32,25 +36,69 @@ Problems
       <tbody>
         @if (Auth::check() and Auth::user()->type == "admin")
           @foreach ($problems as $problem)
+            @foreach ($problem->submissions as $submission)
+              @if ($submission->user_id == Auth::user()->id)
+                @if ($submission->status == "YES")
+                  <?php
+                    $color = "green";
+                    $status = "YES";
+                  ?>
+                  @break
+                @else
+                  <?php
+                    $color = "red";
+                    $status = "NO";
+                  ?>
+                @endif
+              @endif
+            @endforeach
+            <tr>
+              <td scope="row">{{ $problem -> id }}</td>
+              <td>
+                <a href="{{ 'problems/' . $problem -> id }}">{{ $problem -> name }}</a>
+              </td>
+              <td style="{{ 'color: ' . $color . ';' }}">{{ $status }}</td>
+            </tr>
+          @endforeach
+        @elseif (Auth::check() and Auth::user()->type == "student")
+          @foreach ($problems as $problem)
+            @if ($problem -> status === "show")
+              @foreach ($problem->submissions as $submission)
+                @if ($submission->user_id == Auth::user()->id)
+                  @if ($submission->status == "YES")
+                    <?php
+                      $color = "green";
+                      $status = "YES";
+                    ?>
+                    @break
+                  @else
+                    <?php
+                      $color = "red";
+                      $status = "NO";
+                    ?>
+                  @endif
+                @endif
+              @endforeach
               <tr>
                 <td scope="row">{{ $problem -> id }}</td>
                 <td>
                   <a href="{{ 'problems/' . $problem -> id }}">{{ $problem -> name }}</a>
                 </td>
-                <td style="color: green;">{{ $problem -> status }}</td>
+                <td style="{{ 'color: ' . $color . ';' }}">{{ $status }}</td>
               </tr>
+            @endif
           @endforeach
         @else
           @foreach ($problems as $problem)
-              @if ($problem -> status === "show")
-                <tr>
-                  <td scope="row">{{ $problem -> id }}</td>
-                  <td>
-                    <a href="{{ 'problems/' . $problem -> id }}">{{ $problem -> name }}</a>
-                  </td>
-                  <td style="color: green;">{{ $problem -> status }}</td>
-                </tr>
-              @endif
+            @if ($problem -> status === "show")
+              <tr>
+                <td scope="row">{{ $problem -> id }}</td>
+                <td>
+                  <a href="{{ 'problems/' . $problem -> id }}">{{ $problem -> name }}</a>
+                </td>
+                <td style="{{ 'color: ' . $color . ';' }}">{{ $status }}</td>
+              </tr>
+            @endif
           @endforeach
         @endif
       </tbody>
