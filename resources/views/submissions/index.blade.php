5230c49017773e8d203e7882f62afbf4a98b190a
jsice <wiwadh.c@ku.th>
2019-02-05 11:04:23 +0700

    add Submissions Filter

--- resources/views/submissions/index.blade.php
+++ resources/views/submissions/index.blade.php
@@ -3,6 +3,12 @@
   All Submissions
 @endsection
 @section('content')
+  <p>สามารถใช้ตัวกรองหลายตัวได้โดยคั่นด้วย |(or) หรือ &(and)</p>  
+  <div id="filter-error" class="alert alert-danger">
+    ไม่สามารถใช้ | กับ & พร้อมกันได้
+  </div>
+  <input class="form-control" id="submissions-filter" type="text" placeholder="Search..">
+  <br>
   <table class="table table-hover">
     <thead>
       <tr class="table-dark">
@@ -12,7 +18,7 @@
         <th scope="col">Status</th>
       </tr>
     </thead>
-    <tbody>
+    <tbody id="submissions-table">
       @foreach ($submissions as $submission)
         <tr>
           <td scope="row">
@@ -33,3 +39,31 @@
   </table>
   <div style="display: flex; justify-content: center;">{{ $submissions->links() }}</div>
 @endsection
+@push("js")
+<script>
+
+$(document).ready(function(){
+  $("#filter-error").toggle(false);
+  $("#submissions-filter").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#filter-error").toggle((value.includes("|") && value.includes("&")));
+    if (value.includes("|") && value.includes("&")) {
+      return;
+    }
+    var cmd = value.includes("|") ? 2 : (value.includes("&") ? 1 : 0);
+    var values = $(this).val().toLowerCase().split(/\&|\|/);
+    $("#submissions-table tr").filter(function() {
+      let result = null
+      values.forEach(v => {
+        if (result === null) {
+          result = $(this).text().toLowerCase().indexOf(v) > -1
+        } else {
+          result = cmd == 2 ? ($(this).text().toLowerCase().indexOf(v) > -1) || result : ($(this).text().toLowerCase().indexOf(v) > -1) && result
+        }
+      });
+      $(this).toggle(result);
+    });
+  });
+});
+</script>
+@endpush
